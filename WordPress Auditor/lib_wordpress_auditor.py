91a27ca2e90a0c654b0aa5a09e11846c54a4c40b
0pc0deFR <0pc0deFR@gmail.com>
2013-06-20 09:23:19 +0200

    Update WordPresss Auditor 2.0. Add CRSF detection

--- WordPress Auditor/lib_wordpress_auditor.py
+++ WordPress Auditor/lib_wordpress_auditor.py
@@ -10,10 +10,10 @@ def main():
 	plugin = sys.argv[1]
 	content_file = load_php(plugin)
 	if content_file:
-		print "Audit file: " + plugin
+		print "\nAudit file: " + plugin + "\n"
 		auditing(content_file)
 	else:
-		print "The file does not exist"
+		print "\nThe file does not exist!"
 	
 def load_php(plugin):
 	if os.path.isfile(plugin):
@@ -25,12 +25,24 @@ def load_php(plugin):
 		return 0
 
 def auditing(content_file):
-	strings = ["$wpdb->prepare","$wpdb->get_results","$wpdb->query","move_uploaded_file"]
-	i = 0
-	while i < len(strings):
-		if content_file.find(strings[i]) != -1:
-			print strings[i] + " founded"
+	strings_sqli = ["$wpdb->prepare","$wpdb->get_results","$wpdb->query"]
+	strings_csrf = ["wp_create_nonce", "wp_verify_nonce"]
+	i = sql = csrf = 0
+	while i < len(strings_sqli):
+		if content_file.find(strings_sqli[i]) != -1:
+			sql = 1
 		i += 1
 
-		
+	if content_file.find("<form") != -1:
+		i = 0
+		csrf = 1
+		while i < len(strings_csrf):
+			if content_file.find(strings_csrf[i]) == 1:
+				csrf = 0
+			i += 1
+
+	if sql == 1:
+		print "Your plugin is potentially vulnerable to SQL Injection. For more informations: http://en.wikipedia.org/wiki/SQL_injection"
+	if csrf == 1:
+		print "Your plugin is potentially vulnerable to CSRF. For more informations: http://en.wikipedia.org/wiki/Cross-site_request_forgery"
 	return 0
--- WordPress Auditor/wordpress_auditor.py
+++ WordPress Auditor/wordpress_auditor.py
@@ -2,8 +2,8 @@
 
 from lib_wordpress_auditor import *
 
-print "Wordpress Auditor"
+print "\nWordpress Auditor"
 print "Kevin Falcoz (aka 0pc0deFR)"
 print "License GPL"
-Print "V1.0"
+print "V2.0"
 main()
